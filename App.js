0c95732f81227f600245cbd19dbf4fe71b563e7c
clarawox <clarawox@hotmail.com>
2023-06-16 19:55:01 -0300

    [Maria] Ultimo commit do semestre

--- App.js
+++ App.js
@@ -1,84 +1,83 @@
 import React from 'react';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StatusBar, SafeAreaView, View } from 'react-native';
-import {
-  useFonts,
-  Montserrat_400Regular,
-  Montserrat_700Bold,
-} from '@expo-google-fonts/montserrat';
+import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
-import Cesta from './src/telas/Cesta';
-import Sacolao from './src/telas/Sacolao';
-import mock from './src/mocks/cesta';
-import Imagens from './src/telas/Imagens';
+import mock from './src/mocks/Carrossel';
+import Imagens from './telas/Cesta';
+import SobreNos from './telas/SobreNos';
+import Produtos from './telas/Produtos'
 
-function MenuCesta(){
-  return <SafeAreaView>
-            <Cesta {...mock}/>
-            <StatusBar/>
-         </SafeAreaView>;
-}
+// function Inicio() {
+//   return <SafeAreaView>
+//             <Imagens {...mock} />
+//             <StatusBar />
+//           </SafeAreaView>;
+// }
 
 const Tab = createBottomTabNavigator();
 
-function TabsMenu(){
-  return(
+function TabsMenu() {
+  return (
     <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+      screenOptions={({ route }) => ({
+        tabBarStyle:{
+          backgroundColor: "#660066",
+        },
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
 
-            if (route.name === 'Início') {
-              iconName = focused
-                ? 'paw'
-                : 'paw-outline';
-            } else if (route.name === 'Produtos') {
-              iconName = focused 
-                ? 'nutrition' 
-                : 'nutrition-outline';
-            } else if (route.name === 'Minha cesta') {
-              iconName = focused
-                ? 'basket'
-                : 'basket-outline';
-            } else if (route.name === 'Sobre nós') {
-              iconName = focused 
-                ? 'camera' 
-                : 'camera-outline';
-            }
-
-            // You can return any component that you like here!
-            return <Ionicons name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: 'purple',
-          tabBarInactiveTintColor: 'gray',
-        })}
-      >
-        <Tab.Screen name="Inicio" component={MenuCesta} /> 
-        <Tab.Screen name="Produtos" component={Sacolao} /> 
-        <Tab.Screen name="Cesta" component={Imagens} /> 
-      </Tab.Navigator>
+          if (route.name === 'Inicio') {
+            iconName = focused
+              ? 'paw'
+              : 'paw-outline';
+           } else if (route.name === 'Produtos') {
+            iconName = focused
+              ? 'grid'
+              : 'grid-outline';
+          } else if (route.name === 'Minha cesta') {
+            iconName = focused
+              ? 'basket'
+              : 'basket-outline';
+          } else if (route.name === 'Sobre nós') {
+            iconName = focused
+              ? 'book'
+              : 'book-outline';
+          }
+          // You can return any component that you like here!
+          return <Ionicons name={ iconName } size={ size } color={ color } />;
+        },
+        tabBarActiveTintColor: 'white',
+        tabBarInactiveTintColor: 'pink',
+      })}
+    >
+      <Tab.Screen name="Inicio" component={Imagens} />
+      <Tab.Screen name="Sobre nós" component={SobreNos} />
+      <Tab.Screen name="Produtos" component={Produtos} />
+    </Tab.Navigator>
   );
 }
 
 export default function App() {
 
-//Fonte utilizada para o projeto
-const [ fonteCarregada ] = useFonts({
-  "MontSerratRegular" : Montserrat_400Regular,
-  "MontSerratBold" : Montserrat_700Bold,
-});
+  //Fonte utilizada para o projeto
+  const [fonteCarregada] = useFonts({
+    "MontSerratRegular": Montserrat_400Regular,
+    "MontSerratBold": Montserrat_700Bold,
+  });
 
   //Checa se as fontes já foram carregadas antes de exibir no APP
-  if(!fonteCarregada) {
+  if (!fonteCarregada) {
     return <View />;
   }
 
   return (
     <NavigationContainer>
-      <TabsMenu/>
+      <StatusBar style="light" backgroundColor="#FFF" translucent={true}/>
+      <TabsMenu />
     </NavigationContainer>
   );
 }
-
